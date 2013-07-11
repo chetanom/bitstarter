c735ea7d91a1191c7c68a7d30e552c928a719e3c
Chetan Mangala <chetanom@gmail.com>
2013-07-11 22:40:43 +0000

    added web2.js and index2.html

---
+++ web2.js
@@ -0,0 +1,16 @@
+fs = require("fs");
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var buffer = fs.readFileSync("./index.html", "utf-8");
+
+app.get('/', function(request, response) {
+  response.send(buffer);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
